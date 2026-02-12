aaf998a888823a117d78085b806dc32571bd5275
novosadnazar <163895331+novosadnazar@users.noreply.github.com>
2026-02-12 19:27:20 +0200

    wefghe

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,21 +1,22 @@
-import PropTypes from "prop-types"; // ES6
+import style from"./TransactionHistory.module.css"
+import PropTypes from "prop-types"; 
 export const TransactionHistory = ({ items }) => {
   return (
-    <table className="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+    <table className={style.history}>
+      <thead className={style.thead}>
+        <tr className={style.list}>
+          <th className={style.item}>Type</th>
+          <th className={style.item}>Amount</th>
+          <th className={style.item}>Currency</th>
         </tr>
       </thead>
 
-      <tbody>
+      <tbody className={style.tbody}>
         {items.map(({ id, type, amount, currency }) => (
-          <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+          <tr key={id} className={style.list2}>
+            <td className={style.item2}>{type}</td>
+            <td className={style.item2}>{amount}</td>
+            <td className={style.item2}>{currency}</td>
           </tr>
         ))}
       </tbody>
